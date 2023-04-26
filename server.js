d162a9d960a996ae81e2214a2e66d3e213c010af
Tim Maupin <timmaupin@live.com>
2023-04-26 15:34:08 -0500

    set up and finish backend

--- server.js
+++ server.js
@@ -11,21 +11,24 @@ app.use(cors());
 const PORT = process.env.PORT || 5005;
 
 app.get('/', (request, response) => {
-  response.send('helle World');
+  response.send('Hello World');
 });
 
 
 app.get('/weather', (request, response) => {
   try {
     let searchQuery = request.query.searchQuery;
+    console.log('Names !!!', searchQuery);
     let lat = request.query.lat;
     let lon = request.query.lon;
 
-    let dataToInstant = weatherData.find(weather => weather.searchQuery === searchQuery);
-    let dataToSend = new Weather(dataToInstant);
+    let dataToSearchQuery = weatherData.find(weather => weather.city_name === searchQuery);
+    // let dataToLat = weatherData.find(weather => weather.lat === lat);
+    // let dataToLon = weatherData.find(weather => weather.lon === lon);
+    let dataToSend = new Forecast(dataToSearchQuery);
     response.send(dataToSend);
   } catch (error) {
-    next(error);
+    // next(error);
 
   }
 });
@@ -35,6 +38,13 @@ app.get('/weather', (request, response) => {
 
 
 
+class Forecast {
+  constructor(forecastObject){
+    console.log('It seems like its raining: ',forecastObject);
+    this.description = forecastObject.data[0].weather.description;
+    this.date = forecastObject.data[0].valid_date;
+  }
+}
 
 
 
@@ -55,8 +65,9 @@ app.get('/weather', (request, response) => {
 
 
 
-
-
+// app.use((error, request, response, next) => {
+//     response.status(500).send(error.message);
+// });
 
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
