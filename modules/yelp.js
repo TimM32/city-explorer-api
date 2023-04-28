fdd3307b62a1f52a4a7cbb56b4adcc7aefa0d092
Tim Maupin <timmaupin@live.com>
2023-04-28 15:40:48 -0500

    finish yelp review

--- modules/yelp.js
+++ modules/yelp.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 module.exports = getYelp;
 
-function getYelp(location, page) {
+function getYelp(location, page=1) {
   const numPerPage = 4;
   const start = ((page - 1) * numPerPage + 1);
 
@@ -19,7 +19,7 @@ function getYelp(location, page) {
 function parseYelpData(data) {
   try {
     const yelpSummaries = data.businesses.map(business => {
-      return new yelpSummaries(business);
+      return new Yelp(business);
     });
     return Promise.resolve(yelpSummaries);
   } catch(err) {
@@ -27,3 +27,14 @@ function parseYelpData(data) {
   }
 }
 
+class Yelp {
+  constructor(business) {
+    this.tableName = 'yelps';
+    this.name = business.name;
+    this.image_url = business.image_url;
+    this.price = business.price;
+    this.rating = business.rating;
+    this.url = business.url;
+    this.timestamp = Date.now();
+  }
+}
