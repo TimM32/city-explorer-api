1ea3526797be7dd8fa6f98e91e3ad646c98481d9
Tim Maupin <timmaupin@live.com>
2023-04-28 15:47:33 -0500

    add async getweather function to the weather.js

--- server.js
+++ server.js
@@ -19,40 +19,14 @@ app.get("/weather", getWeather);
 app.get('/movies', getMovies);
 // app.get('/yelp', getYelp);
 
-async function getWeather(request, response) {
-  let { latitude, longitude } = request.query;
-  // console.log(latitude, longitude);
-  //make api call to get actual weather data
-  let url = `https://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&days=5&aqi=no&alerts=no&q=${latitude},${longitude}`;
 
-  let weatherData = await axios.get(url);
-  console.log('YYYYY', weatherData.data.forecast.forecastday);
-  let weatherSummaries = weatherData.data.forecast.forecastday.map(day => {
-    return new Forecast(day);
-  });
-  console.log('WWWWWWWSSSSSSs', weatherSummaries);
-  response.status(200).send(weatherSummaries);
-}
-
-
-
-
-class Forecast {
-  constructor(forecastObject) {
-    console.log('CCCCC', forecastObject.day.condition.text);
-    // this.description = forecastObject.weather.description;
-    this.date = forecastObject.date;
-    this.forecast = forecastObject.day.condition.text;
-
-  }//constructor
-}//class
 
 
 async function getMovie(request, response) {
   let searchQuery = request.query.searchQuery;
   let
 
-  
+
 }
 
 
