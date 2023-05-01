6f4e0ed3f1aa609318d44ae55372c5040ffb6ec8
Tim Maupin <timmaupin@live.com>
2023-05-01 08:16:46 -0500

    finishing up the movie.js

--- modules/movie.js
+++ modules/movie.js
@@ -1,11 +1,18 @@
-// 'use strict';
-// https://api.themoviedb.org/3/movie/550?api_key=86f7a50af008e2990fa03c9bf07cdb53
+'use strict';
 
-// const axios = require('axios');
 
-// module.exports = getMovie;
+const axios = require('axios');
 
-// async function getMovies(request, response) {
-//     let 
-//     let url = ``
-// }
+module.exports = getMovie;
+
+async function getMovies(request, response) {
+    let { lat, lon } = request.query;
+    let url = `https://api.themoviedb.org/3/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=seattle&page=1&include_adult=false`;
+
+    let movieCity = await.axios.get(url)
+    let movieArray = movieCity.data.movieData.map(movie => new movie(movieData));
+    response.status(200).send(movieArray)
+}
+
+
+class Movie 
