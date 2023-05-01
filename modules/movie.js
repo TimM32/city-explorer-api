a49f86c3e8e17596c99d8c846e5c3e86dcd6987f
Tim Maupin <timmaupin@live.com>
2023-05-01 10:09:19 -0500

    small changes to help make code work better

--- modules/movie.js
+++ modules/movie.js
@@ -5,12 +5,12 @@ const axios = require('axios');
 
 module.exports = getMovie;
 
-async function getMovies(request, response) {
+async function getMovie(request, response) {
     let cityMovieSearch = request.query;
     let url = `https://api.themoviedb.org/3/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${cityMovieSearch}&page=1&include_adult=false`;
 
-    let movieCity = await.axios.get(url)
-    let movieArray = movieCity.data.movieData.map(movie => new movie(movieData));
+    let movieCity = await axios.get(url)
+    let movieArray = movieCity.data.results.map(movie => new Movie(movie));
     response.status(200).send(movieArray)
 }
 
