fa15bc4ca1119aed55aff099af07600a5a313771
Tim Maupin <timmaupin@live.com>
2023-05-01 10:03:59 -0500

    add let city fuction

--- modules/movie.js
+++ modules/movie.js
@@ -6,8 +6,8 @@ const axios = require('axios');
 module.exports = getMovie;
 
 async function getMovies(request, response) {
-    let { lat, lon } = request.query;
-    let url = `https://api.themoviedb.org/3/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=seattle&page=1&include_adult=false`;
+    let cityMovieSearch = request.query;
+    let url = `https://api.themoviedb.org/3/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${cityMovieSearch}&page=1&include_adult=false`;
 
     let movieCity = await.axios.get(url)
     let movieArray = movieCity.data.movieData.map(movie => new movie(movieData));
